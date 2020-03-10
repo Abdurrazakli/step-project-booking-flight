ab1fe1b1f874096c19860fea61a0b05a002ae34c
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 00:03:11 +0400

    Console class created for reading and writing operations with console

---
+++ src/main/java/util/Console.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.Scanner;
+
+public class Console {
+    public void print(String line){
+        System.out.println(line);
+    }
+    public String read(){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next().trim();
+        return input;
+    }
+    public String readLine(){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().trim();
+        return input;
+    }
+}
