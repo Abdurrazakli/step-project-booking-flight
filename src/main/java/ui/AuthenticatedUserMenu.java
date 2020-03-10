b73260b6cfc79077835ce963a20d21c75cc5c8d8
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 00:44:47 +0400

    '.' removed from message

--- src/main/java/ui/AuthenticatedUserMenu.java
+++ src/main/java/ui/AuthenticatedUserMenu.java
@@ -10,7 +10,7 @@ public class AuthenticatedUserMenu implements Menu {
         sb.append("|--------------------------------------------|\n");
         sb.append("|Please choose the id of operation:          |\n");
         sb.append("|                                            |\n");
-        sb.append("|1. Show all flights.                        |\n");
+        sb.append("|1. Show all flights                         |\n");
         sb.append("|2. Show the flight info by flightNumber     |\n");
         sb.append("|3. Search a flight                          |\n");
         sb.append("|4. Book a flight                            |\n");
