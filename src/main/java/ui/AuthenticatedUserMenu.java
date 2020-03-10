24408bc6b0d87631102422b0381d1c2445873bb8
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-10 23:24:19 +0400

    AuthenticatedUserMenu added

---
+++ src/main/java/ui/AuthenticatedUserMenu.java
@@ -0,0 +1,23 @@
+package ui;
+
+public class AuthenticatedUserMenu implements Menu {
+
+    @Override
+    public String show() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|============================================|\n");
+        sb.append("|Welcome to the airport ticket booking app!  |\n");
+        sb.append("|--------------------------------------------|\n");
+        sb.append("|Please choose the id of operation:          |\n");
+        sb.append("|                                            |\n");
+        sb.append("|1. Show all flights.                        |\n");
+        sb.append("|2. Show the flight info by flightNumber     |\n");
+        sb.append("|3. Search a flight                          |\n");
+        sb.append("|4. Book a flight                            |\n");
+        sb.append("|5. Cancel booking                           |\n");
+        sb.append("|6. My flights                               |\n");
+        sb.append("|7. Exit                                     |\n");
+        sb.append("|============================================|\n");
+        return sb.toString();
+    }
+}
