82b668bfcfbf1548279f8c857a08cbf3f04b6f8c
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-12 00:10:38 +0400

    GuestController is ready

--- src/main/java/Controller/GuestController.java
+++ src/main/java/Controller/GuestController.java
@@ -1,13 +1,53 @@
 package Controller;
 
+
+import exceptions.InvalidInput;
+import service.FlightService;
 import ui.Console;
 import ui.Messages;
+import util.TerminateProgram;
 
 public class GuestController {
+    FlightService flightService = new FlightService();
+    FlightController flightController = new FlightController();
+    TerminateProgram terminator = new TerminateProgram();
     Console console = new Console();
     public void start(){
-        console.print(Messages.showGuestMenu());
-
-        throw new IllegalArgumentException("GuestControllerStart implement!");
+        int choice = 0;
+        int operations_count = 4;
+        while (true) {
+            try{
+                console.print(Messages.showGuestMenu());
+                choice = Integer.parseInt(console.readLine());
+                if(choice > operations_count || choice < 0){
+                    throw new InvalidInput("Invalid input of user");
+                }
+                else {
+                    switch (choice){
+                        case 1:
+                            flightController.showAllFlights();
+                        break;
+                        case 2:
+                            console.print("\nPlease enter flight number:(ex THY358)");
+                            flightController.showAllFlightByFlightNumber(console.readLine());
+                            break;
+                        case 3:
+                            flightController.showSearchedFlight();
+                            break;
+                        case 4:
+                            terminator.terminate();
+                            break;
+                        default:break;
+                    }
+                }
+            }
+            catch (NumberFormatException ex){
+                    console.print("Only numbers");
+            }
+            catch (InvalidInput ex){
+                console.print(ex.getMessage());
+            }
+        }
     }
+
 }
