5a5ee0305782a2ba3f974aaf1487a65fe94f4a6f
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 17:54:13 +0400

    login controller features

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -1,7 +1,21 @@
 package Controller;
 
+import entity.User;
+import service.LoginService;
+import ui.Console;
+
+import java.util.Optional;
+
 public class LoginController {
+    LoginService loginService = new LoginService();
+    Console console = new Console();
+    String username;
+    String password;
     public boolean login(){
-        throw new IllegalArgumentException("Not impl");
+        console.print("Please enter your username: ");
+        username = console.readLine();
+        console.print("Please enter your password: ");
+        password = console.readLine();
+        throw new RuntimeException("Yes");
     }
 }
