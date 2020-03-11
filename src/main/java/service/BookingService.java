8b00605334e638eeaf214693bd36aef6df6f1f61
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 20:16:40 +0400

    Structured

--- src/main/java/service/BookingService.java
+++ src/main/java/service/BookingService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dao.BookingDAO;
+import entity.Booking;
+
+import java.util.List;
+
 public class BookingService {
+    private BookingDAO dao = new BookingDAO();
+    public List<Booking> getAllBookings(){
+        return dao.getAll();
+    }
+    public List<Booking> getFlightsByFullName(String name, String surname){
+        //todo booking by full name service
+        //Should return booked flights by full name or this full name is passenger of that flight
+        throw new IllegalArgumentException("Booking full name implement!");
+    }
+    public boolean bookAFlight(Booking booking){
+        //todo book a flight service
+        throw new IllegalArgumentException("booking made implement");
+    }
+    public boolean cancelBooking(Booking booked){
+        //todo delete specified booking service
+        throw new IllegalArgumentException("cancel booking implement");
+    }
 }
--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -5,7 +5,13 @@ import java.util.Optional;
 
 public class LoginService {
     private UserService userService = new UserService();
-
+    /*
+        todo login boolean qaytarsin.
+     Loginle bagli butun prosedurlar bu loginin icinde bas versin
+     message gostermek
+     menyu secimlerini etmek
+     onlara uygun lazimi servisleri cagirmaq
+     */
     public Optional<User> login(String username, String password){
         return userService.checkUsernameAndPassword(username,password);
     }
