d2f137e54a2a1454204d04bb7aab70067f3290c9
Afgan Mustafayev <efqan.mustafayevstd99@gmail.com>
2020-03-11 17:17:38 +0400

    -- LoginService changed

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -9,7 +9,14 @@ public class UserService {
     private UserDAO userDAO = new UserDAO();
 
     public boolean checkUsername(String username){
-        return userDAO.getAll().stream().filter(user -> user.surname.equals(username)).toArray().length == 0;
+        return userDAO.getAll().stream().filter(user -> user.username.equals(username)).toArray().length == 0;
+    }
+    public Optional<User> checkUsernameAndPassword(String username, String password){
+        Optional<User> user = userDAO.getAll().stream().filter(user1 -> user1.username.equals(username)).findAny();
+        if(user.isPresent() && user.get().password.equals(password)){
+            return user;
+        }
+        return Optional.empty();
     }
     public boolean createNewUser(User user){
         return userDAO.create(user);
