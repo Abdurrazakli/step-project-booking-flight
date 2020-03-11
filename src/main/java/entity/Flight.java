452569854465cac0f7668e5323a87a71596496f1
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 23:40:28 +0400

    Representation is more user friendly

--- src/main/java/entity/Flight.java
+++ src/main/java/entity/Flight.java
@@ -25,7 +25,7 @@ public class Flight implements Serializable {
     }
 
     private String represent(){
-        return String.format("%s %s %s %d %d %s", flightNumber, from, to, price, seats, flightDate);
+        return String.format("%s %s-->%s %dAZN %d %s", flightNumber, from, to, price, seats, flightDate);
     }
     @Override
     public String toString() {
