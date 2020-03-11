7abec836f271cf18a4d0675cbf813feb76e28c7e
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-11 22:40:28 +0400

    Show all flights added

--- src/main/java/Controller/FlightController.java
+++ src/main/java/Controller/FlightController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import entity.Flight;
 import service.FlightService;
 import ui.Console;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class FlightController {
     private FlightService flightService = new FlightService();
@@ -12,5 +17,13 @@ public class FlightController {
                 .getFlights()
                 .stream().
                 forEach(f->console.print(f.toString()));
+    }
+    public void showAllFlightByFlightNumber(String FN){
+        List<Flight> filtered = flightService.getFlightsByFlightNumber(FN);
+        if(filtered.size() == 0){
+            console.print("There is not such a flight.\nMake sure it is valid FN");
+        }else {
+            filtered.stream().forEach(f->console.print(f.toString()));
+        }
     }
 }
