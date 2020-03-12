f38029eb180d8b75b1e82165294d6c3a6d53fd34
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-12 14:09:35 +0400

    Tests

---
+++ src/test/java/service/BookingServiceTest.java
@@ -0,0 +1,29 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceTest {
+
+    @Test
+    void getAllBookings() {
+
+    }
+
+    @Test
+    void getFlightsByFullName() {
+    }
+
+    @Test
+    void bookAFlight() {
+    }
+
+    @Test
+    void getNextID() {
+    }
+
+    @Test
+    void cancelBooking() {
+    }
+}
