0366e82ace140b5ae1d2635b0c262f468101b4e1
Adil Abdurrazaklı <abdurrazakliadil@gmail.com>
2020-03-12 14:28:22 +0400

    tests errors

--- src/test/java/service/BookingServiceTest.java
+++ src/test/java/service/BookingServiceTest.java
@@ -1,10 +1,25 @@
 package service;
 
+import entity.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingServiceTest {
+    List<Booking> bookings;
+    @BeforeEach
+    void init(){
+        Flight f = new Flight(12,"THY358", Airport.ATL,Airport.AGD,100,10,LocalDate.now());
+        List<Passenger> passengers = new ArrayList<>();
+        passengers.add(new Passenger("p1name","p1surname"));
+        User user = new User("uname","u")
+        Booking b1 = new Booking(1,f,passengers,)
+    }
 
     @Test
     void getAllBookings() {
